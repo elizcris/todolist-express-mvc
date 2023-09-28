b94e4b92c9970335d01b46ed1f34fe0e55078201
Elizabeth Cristina <lizzy.cristina@gmail.com>
2023-09-28 08:15:24 -0500

    Comment routes/home.js file

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,8 @@
+//router file
 const express = require('express')
 const router = express.Router()
 const homeController = require('../controllers/home')
 
-router.get('/', homeController.getIndex) 
+router.get('/', homeController.getIndex) //all of the root route get requests use the 'homeController' constant that runs the 'controllers/home.js' file to use the getIndex method
 
 module.exports = router
